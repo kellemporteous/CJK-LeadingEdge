ecf98748002b8a6a038a1e757cbbd84a15f2e2b0
SAE\kporteous <1005781@student.sae.edu.au>
2017-03-20 17:02:44 +1000

    I've added player movement and animtated the kite (still being tweeked). Gravity is working fine but needs to be fine tuned - Kellem Porteous

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public Rigidbody2D rb;
+    public float moveSpeed;
+  
+
+
+	// Use this for initialization
+	void Start () {
+
+        rb = GetComponent<Rigidbody2D>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void FixedUpdate()
+    {
+        Controls();
+    }
+
+    void Controls()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
+
+        rb.AddForce(movement * moveSpeed);
+    }
+}
