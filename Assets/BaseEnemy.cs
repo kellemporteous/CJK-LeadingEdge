f16be8c5e68607e101c52e5f6769eea97cd1aaa5
Kellem Porteous <1005781@student.sae.edu.au>
2017-04-09 21:46:20 +1000

    Added some basic stuff for the game, half done broken code. will ask steve for help

---
+++ Assets/BaseEnemy.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseEnemy : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    protected virtual void movement()
+    {
+
+    }
+}
--- Assets/CameraControls.cs
+++ Assets/CameraControls.cs
@@ -7,16 +7,33 @@ public class CameraControls : MonoBehaviour {
     //necessary varibles go here
     public GameObject player;
 
-	// Use this for initialization
-	void Start ()
-    {
+    public float leftConstraint = Screen.width;
+    public float rightConstraint = Screen.width;
+    public float buffer = 2.0f;
+    public float distZ;
 
+    Camera cam;
+
+    void Awake()
+    {
+        cam = Camera.main;
+        distZ = Mathf.Abs(cam.transform.position.z + transform.position.z);
+
+        leftConstraint = cam.ScreenToWorldPoint(new Vector3(0, 0, distZ)).x;
+        rightConstraint = cam.ScreenToWorldPoint(new Vector3(Screen.width, 0, distZ)).x;
+    }
+    // Use this for initialization
+    void Start ()
+    {
         // this is so that the player is attached to the correct gameobject
-        player = GameObject.FindGameObjectWithTag("Player");	
-	}
-	
-	// Update is called once per frame
-	void Update ()
+        player = GameObject.FindGameObjectWithTag("Player");
+
+      
+    }
+
+
+    // Update is called once per frame
+    void Update ()
     {
         if (player.transform.position.y > 0)
         {
@@ -25,5 +42,14 @@ public class CameraControls : MonoBehaviour {
                 this.transform.position = new Vector3(transform.position.x, player.transform.position.y - 4.5f, -10);
             }
         }
-	}
+
+        if (transform.position.x < leftConstraint - buffer)
+        {
+            transform.position = new Vector3(rightConstraint + buffer, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x > rightConstraint + buffer)
+        {
+            transform.position = new Vector3(leftConstraint - buffer, transform.position.y, transform.position.z);
+        }
+    }
 }
