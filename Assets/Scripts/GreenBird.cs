7772a9b6e2cc6c3a305fc9c38b082ac289e99417
SAE\kporteous <1005781@student.sae.edu.au>
2017-04-18 11:38:44 +1000

    Started adding new/ updated sprites

--- Assets/Scripts/GreenBird.cs
+++ Assets/Scripts/GreenBird.cs
@@ -14,7 +14,7 @@ void FixedUpdate()
 
     protected override void Movement()
     {
-        transform.position = Vector2.MoveTowards(transform.position, playeSr.transform.position, moveSpeed * Time.deltaTime);
+        transform.position = Vector2.MoveTowards(transform.position, player.transform.position, moveSpeed * Time.deltaTime);
         //this.gameObject.transform.LookAt(new Vector3(0.0f, 0.0f, target.transform.position.z));
         //transform.position += transform.forward * moveSpeed * Time.deltaTime;
     }
